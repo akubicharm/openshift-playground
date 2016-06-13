003aee7ef5a10a67b50114ddd3f67168b26fa05e
Kei Omizo <komizo@redhata.com>
2016-06-13 18:26:15 +0900

    update -pool parameters

--- install3.2/install.sh
+++ install3.2/install.sh
@@ -13,7 +13,7 @@ done
 for svr in $SERVERS ; do
 echo $svr
 vagrant ssh $svr --command "sudo subscription-manager register --username=<RHN_USERNAME> --password=<RHN_PASSWORD>"
-vagrant ssh $svr --command "sudo subscription-manager attach --pool <POOL_ID>"
+vagrant ssh $svr --command "sudo subscription-manager attach --pool=<POOL_ID>"
 vagrant ssh $svr --command "sudo subscription-manager repos --disable='*'; sudo subscription-manager repos --enable='rhel-7-server-rpms' --enable='rhel-7-server-extras-rpms' --enable='rhel-7-server-ose-3.2-rpms'"
 vagrant ssh $svr --command "sudo yum -y install wget git net-tools bind-utils iptables-services bridge-utils bash-completion"
 vagrant ssh $svr --command "sudo yum -y update"
